6d813d06101b6ef547fca9d8d96b8dcbb8695ca3
jonas554422 <152663366+jonas554422@users.noreply.github.com>
2024-03-01 21:42:24 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,6 +1,6 @@
 import re
 from tinydb import TinyDB, Query
-from datetime import datetime
+from datetime import datetime, timedelta
 from refresh_mci import aktualisiere_mci_daten
 import locale
 import matplotlib.pyplot as plt
@@ -34,7 +34,7 @@ class UserDatabase:
     
 
     def is_room_available(self, room_number, date, start_time, end_time):
-        date_format = "%A, %d.%m.%Y"  # Aktualisiert, um den Wochentag zu berücksichtigen
+        date_format = "%A, %d.%m.%Y"  
         time_format = "%H:%M"
         try:
             date_obj = datetime.strptime(date, date_format).date()
@@ -42,7 +42,7 @@ class UserDatabase:
             end_time_obj = datetime.strptime(end_time, time_format).time()
         except ValueError as e:
             print(f"Fehler beim Parsen des Datums oder der Uhrzeit: {e}")
-            return False  # oder entsprechende Fehlerbehandlung
+            return False  # Wenn ein Fehler auftritt, ist der Raum nicht verfügbar
 
         availabilities = self.available_rooms_db.search(Query().Raumnummer == room_number)
 
@@ -53,11 +53,11 @@ class UserDatabase:
                 avail_end_time_obj = datetime.strptime(available['Verfuegbar bis'], time_format).time()
             except ValueError as e:
                 print(f"Fehler beim Parsen des Datums oder der Uhrzeit in Verfügbarkeiten: {e}")
-                continue  # oder entsprechende Fehlerbehandlung
+                continue  
 
             if date_obj == avail_date_obj and start_time_obj >= avail_start_time_obj and end_time_obj <= avail_end_time_obj:
                 return True
-        return False
+        return False # Wenn keine passende Verfügbarkeit gefunden wurde, ist der Raum nicht verfügbar
         
     def get_reservations_for_room(self, room_number):
         """Gibt alle Reservierungen für einen bestimmten Raum zurück."""
@@ -67,7 +67,7 @@ class UserDatabase:
     def add_reservation(self, email, room_number, date, start_time, end_time):
         if not self.is_room_available(room_number, date, start_time, end_time):
             return False, "Raum ist zu diesem Zeitpunkt nicht verfügbar."
-        # Überprüfe, ob die Reservierung bereits existiert
+        
         existing_reservations = self.reservation_table.search((Query().room_number == room_number) & 
                                                            (Query().date == date) & 
                                                            ((Query().start_time < end_time) & (Query().end_time > start_time)))
@@ -189,6 +189,7 @@ class UserDatabase:
         
         # Entfernen der Bewertung aus der Datenbank
         self.db.table('reviews').remove(doc_ids=[review_id])
+        
 
         
         
@@ -217,9 +218,6 @@ class UserDatabase:
         return False
     
     def notify_user_of_cancellation(self, email, room_number, date):
-        # Hier können Sie den Code einfügen, der die Benachrichtigung an den Benutzer sendet,
-        # z. B. per E-Mail, über ein internes Nachrichtensystem usw.
-        # In diesem Beispiel drucken wir einfach eine Nachricht aus.
         print(f"Benachrichtigung: Ihre Reservierung für Raum {room_number} am {date} wurde storniert.")
 
     def cancel_reservation(self, reservation_id):
@@ -240,11 +238,10 @@ class UserDatabase:
             })
             # Lösche die Reservierung
             self.reservation_table.remove(doc_ids=[reservation_id])
-    
+
     def delete_all_user_storno_entries(self, user_email):
         # Lösche alle Stornierungseinträge des Users
         self.storno_table.remove(Query().email == user_email)
-        
 
     def calculate_availability(self, available_times, reservations):   
         new_availability = []
@@ -300,8 +297,8 @@ class UserDatabase:
         #Alle reservierungen aus der Datenbank Holen
         l1 = self.get_all_reservations()
         #Leeres Dict und Leere liste erstellen:
-        dict_1 = {}             #Wir benötigt um die totale Buchungszeit eines Raumes zu bestimmen 
-        l2 = []                 #Liste die nur noch Datum, Raumnummer und Bunchungszeit enthält
+        dict_1 = {}              
+        l2 = []                 
 
         for item in l1:                 #Daten extrahieren
             date = item['date']
@@ -370,6 +367,55 @@ class UserDatabase:
 
             plt.tight_layout()
             st.pyplot(fig)
+        
+    def reminder_reservation(self, user_email):
+        
+        #Aktuelles Datum 
+        current_date = datetime.now().date()        #Aktuelles Datum     
+        #Aktuelle Zeit
+        current_time = datetime.now() # Zeit im format 'datetime.datetime'
+        #Differenz -> wird für einen vergelcih benötigt
+        diff_dauer = timedelta(minutes=5)   #Diff von 5 min
+        #liste mit allen reservierungen des users
+        l1 = self.get_user_reservations(user_email)
+        #rausfiltern Wann die nächste reservierung Vorliegt
+        for i, item in enumerate(l1):
+            #für jedes item einen eigenen session_state anlegen:
+            if f'item_state{i}' not in st.session_state:
+                st.session_state[f'item_state{i}'] = 0
+            #print(f"Wert im State {i}: {st.session_state[f'item_state{i}']}")
+            datum_item = item['date']
+            datum_user_reservation = datetime.strptime(datum_item, '%A, %d.%m.%Y').date()
+            #Zeit mit datum Kombinieren und in format 'datetime.datetime' bringen
+            start_time = datetime.combine(datum_user_reservation, datetime.strptime(item['start_time'],"%H:%M").time()) 
+            end_time = datetime.combine(datum_user_reservation, datetime.strptime(item['end_time'],"%H:%M").time())
+
+            if datum_user_reservation == current_date:
+                if st.session_state[f'item_state{i}'] < 2 and current_time < start_time:
+                    #Beginnt meine Reservierung in den nächsten 5 minuten?
+                    if start_time - current_time <= diff_dauer:
+                        st.toast(f''':green[Ihre Reservierung für den raum {item['room_number']} beginnt inerhalb der nächsten 5 min]''')
+                        #Counter Session State 'func_call um 1 erhöhen 
+                        st.session_state[f'item_state{i}'] +=1
+                        #print(f"Wert im State {i}: {st.session_state[f'item_state{i}']}")
+
+                elif st.session_state[f'item_state{i}']>=2 and st.session_state[f'item_state{i}']<4:         
+                    #Endet meine Reservierung in den nächsten 5 minuten?
+                    if end_time > current_time:
+                        if end_time - current_time <= diff_dauer:
+                            st.toast(f''':red[Ihre Reservierung für den raum {item['room_number']} endet inerhalb der nächsten 5 min]''')
+                            st.session_state[f'item_state{i}']+=1
+                            #print(f"Wert im State {i}: {st.session_state[f'item_state{i}']}")
+                #Counter reseten wenn 4 und end_time überschritten ist:
+                elif st.session_state[f'item_state{i}'] == 4 and current_time >= end_time:
+                    st.session_state[f'item_state{i}'] = 0
+                    #print(f"Reset -> Wert im State{i} : {st.session_state[f'item_state{i}']}")
+                #Spezialfälle
+                # Wenn die Seite neu geladen wurde und die Reservierung bald endet
+                elif st.session_state[f'item_state{i}'] == 0 and current_time < end_time and current_time > start_time:
+                    st.session_state[f'item_state{i}'] =2
+                    #print(f"Quereinstieg -> Wert im State{i} : {st.session_state[f'item_state{i}']}")
+
 
 if __name__ == "__main__":
     db = UserDatabase()
