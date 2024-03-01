46839b8ae63f17c070a0ebff1834e12b3a3f6a1f
jonas554422 <152663366+jonas554422@users.noreply.github.com>
2024-03-01 18:51:33 +0100

    Create login.py

---
+++ login.py
@@ -0,0 +1,3 @@
+username = 'mj5804@mci4me.at' # Hier bitte die MCI4ME-Email-Adresse eintragen
+password = 'SvWerder1899' # Hier bitte das MCI4ME-Passwort eintragen um Daten aus Ihrem MCI-Kalender zu extrahieren
+
