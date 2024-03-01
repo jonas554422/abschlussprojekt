b60385ff9fd3305867f4a809b3c3f0e1b3973c9a
jonas554422 <152663366+jonas554422@users.noreply.github.com>
2024-03-01 22:38:49 +0100

    Update login.py

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
-username = 'mj5804@mci4me.at' # Hier bitte die MCI4ME-Email-Adresse eintragen
-password = 'SvWerder1899' # Hier bitte das MCI4ME-Passwort eintragen um Daten aus Ihrem MCI-Kalender zu extrahieren
+username = 'EMAIL' # Hier bitte die myMCI-Email-Adresse eintragen
+password = 'PASSWORT' # Hier bitte das myMCI-Passwort eintragen um Daten aus Ihrem MCI-Kalender zu extrahieren
 
 code = '123' # Code für den Admin-Bereich, kann hier auch geändert werden
 
